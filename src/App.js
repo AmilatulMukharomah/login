11550c3b364ecfce27067dde63d45d02529f0466
Mila <47048357+MilaKisaragi@users.noreply.github.com>
2021-03-22 14:42:44 +0700

    add css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Content from './Content';
+import { BrowserRouter, Route, Redirect, Switch, useHistory, Link, useLocation } from 'react-router-dom';
 
-function App() {
+const App = () => {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [isLoggedin, setIsloggedin] = useState(false)
+
+  useEffect(() => {
+    alert('Selamat Datang')
+    return () => {
+    }
+  }, [])
+
+  const onLogin = (e) => {
+    e.preventDefault()
+    if (username === 'admin' && password === 'admin') {
+      setIsloggedin(true)
+    } else {
+      alert('Salah')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {isLoggedin ? <Content /> : <Redirect to="/login"></Redirect>}
+          </Route>
+          <Route exact path="/login">
+            {!isLoggedin ?
+              <div className="login">
+                <p className="judul_login">LOGIN</p>
+                <form onSubmit={e => onLogin(e)}>
+                  <div className="email">
+                    <input type="text" name="username" onChange={e => setUsername(e.target.value)}></input>
+                  </div>
+                  <div className="password">
+                    <input type="password" name="password" onChange={e => setPassword(e.target.value)}></input>
+                  </div>
+                  <button>login</button>
+                </form>
+              </div> :
+              <Redirect to="/" />
+            }
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Content.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Content = () => {
+    return (
+        <div>
+            KONTENNYA
+        </div>
+    )
+}
+
+export default Content
